a5003117801e6341c6d5fb0054462326ae27e682
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-22 19:03:40 +0100

    Fatto tutto, select, ecc, rimasto da colorare per tipo.

--- main.js
+++ main.js
@@ -97,17 +97,75 @@ const icons = [{
 ];
 
 
-icons.forEach((item, i, array) => {
-console.log(array.length);
-	$('.template .card i').addClass(`${item.family} ${item.prefix}${item.name}`)
-  let cardClone = $('.template .card').clone()
-  $('div.canvas').append(cardClone);
+const animals = icons.filter((item) => {
+  return item.type == 'animal';
+})
 
+const vegetables = icons.filter((item) => {
+  return item.type == 'vegetable';
+})
+
+const users = icons.filter((item) => {
+  return item.type == 'user';
+})
+
+icons.forEach((item, i, array) => {
+	$('.template .card').html(`<i class="${item.family} ${item.prefix}${item.name}"></i>`);
+	let cardClone = $('.template .card').clone()
+	$('div.canvas').append(cardClone);
 });
 
-let a = $( "select" ).val();
-console.log(a);
-$( "select" )
-  .change(function () {
- console.log($(this).val());
-  })
+$("select").change(function() {
+
+  icons.forEach((item, i, array) => {
+		$('.template .card').html(`<i class="${item.family} ${item.prefix}${item.name}"></i>`);
+    let cardClone = $('.template .card').clone()
+    $('div.canvas').append(cardClone);
+  });
+
+  if ($(this).val() == 'all') {
+
+    icons.forEach((item, i) => {
+      $('.canvas .card').remove();
+    });
+
+    icons.forEach((item, i, array) => {
+			$('.template .card').html(`<i class="${item.family} ${item.prefix}${item.name}"></i>`);
+      let cardClone = $('.template .card').clone()
+      $('div.canvas').append(cardClone);
+    });
+  } else if ($(this).val() == 'animal') {
+
+    icons.forEach((item, i) => {
+      $('.canvas .card').remove();
+    });
+
+    animals.forEach((item, i, array) => {
+			$('.template .card').html(`<i class="${item.family} ${item.prefix}${item.name}"></i>`);
+      let cardClone = $('.template .card').clone()
+      $('div.canvas').append(cardClone);
+    })
+  } else if ($(this).val() == 'vegetable') {
+
+    icons.forEach((item, i) => {
+      $('.canvas .card').remove();
+    });
+
+    vegetables.forEach((item, i, array) => {
+			$('.template .card').html(`<i class="${item.family} ${item.prefix}${item.name}"></i>`);
+      let cardClone = $('.template .card').clone()
+      $('div.canvas').append(cardClone);
+    })
+  } else if ($(this).val() == 'user') {
+
+    icons.forEach((item, i) => {
+      $('.canvas .card').remove();
+    });
+
+    users.forEach((item, i, array) => {
+			$('.template .card').html(`<i class="${item.family} ${item.prefix}${item.name}"></i>`);
+      let cardClone = $('.template .card').clone()
+      $('div.canvas').append(cardClone);
+    })
+  }
+});
